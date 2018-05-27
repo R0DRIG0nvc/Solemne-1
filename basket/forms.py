80a425dbb2fd5ae0de6fe3ea247e0f74542aaed5
asd <asd@asd.com>
2018-05-27 18:44:49 -0400

    Avance CRUD de Player, Team y Coach

--- basket/forms.py
+++ basket/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from basket.models import Player
+from basket.models import Player, Team, Coach
 
 
 class PlayerForm(ModelForm):
@@ -7,3 +7,36 @@ class PlayerForm(ModelForm):
         model = Player
         fields = ['rut', 'dv', 'name', 'nickname', 'birthday', 'age', 'email', 'height', 'weight', 'picture', 'position', 'team']
 
+
+class TeamForm(ModelForm):
+    class Meta:
+        model = Team
+        fields = ['name', 'logo', 'description']
+
+
+class CoachForm(ModelForm):
+    class Meta:
+        model = Coach
+        fields = ['name', 'age', 'email', 'nickname', 'rut', 'dv']
+
+
+class EditPlayerForm(ModelForm):
+    def init(self, args, **kwargs):
+        super(EditPlayerForm, self).init(args, **kwargs)
+        self.fields['picture'].required = False
+
+    class Meta:
+        model = Player
+        fields = ['rut', 'dv', 'name', 'nickname', 'birthday', 'age', 'email', 'height', 'weight', 'picture', 'position', 'team']
+
+
+class EditCoachForm(ModelForm):
+    class Meta:
+        model = Coach
+        fields = ['name', 'age', 'email', 'nickname', 'rut', 'dv']
+
+
+class EditTeamForm(ModelForm):
+    class Meta:
+        model = Team
+        fields = ['name', 'logo', 'description']
